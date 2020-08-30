0b216d614bb521cbb2b2f5189d49b27a3320c6d0
telephonist <telephonist1@yahoo.com>
2020-08-30 18:22:16 +0300

    added android.rs

--- android.rs
+++ android.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use std::process::Command;
 use std::thread;
 use std::time::Duration;
@@ -36,6 +37,38 @@ fn unmount() {
         .expect("unmounting failed");
 }
 
+fn get_process(program: &str) -> String {
+    let output = Command::new("ps")
+        .arg("-fC")
+        .arg(program)
+        .output()
+        .expect("failed to get process");
+    let output = String::from_utf8_lossy(&output.stdout);
+    output.to_string()
+}
+
+fn synchronize(dir: &str, dest_dir: &str) {
+    for file in listdir(dir.to_string()) {
+        if listdir(dest_dir.to_string()).contains(&file) {
+            continue;
+        } else {
+            let mut path = "/home/user/Music/".to_owned();
+            path.push_str(&file);
+            let directory = Path::new(&path);
+            if directory.is_dir() && listdir(format!("/home/user/Music/{}", file)).len() < 1 {
+                continue;
+            } else {
+                Command::new("cp")
+                    .arg("-r")
+                    .arg(format!("{}/{}", &dir, &file))
+                    .arg(&dest_dir)
+                    .spawn()
+                    .expect("Failed to copy file");
+            }
+        }
+    }
+}
+
 fn main() {
     loop {
         if detect().contains("1")
@@ -46,6 +79,11 @@ fn main() {
             && listdir("/home/user/mobile".to_string()).contains(&"Card".to_string())
         {
             unmount();
+        } else if get_process("youtube-dl").contains("youtube-dl") == false
+            && listdir("/home/user/mobile".to_string()).contains(&"Card".to_string())
+            && detect().contains("1")
+        {
+            synchronize("/home/user/Music/", "/home/user/mobile/Card/Music/");
         }
         thread::sleep(Duration::from_secs(10));
     }
